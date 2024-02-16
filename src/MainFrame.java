30cce2db112d7d6e2c5c21503cf2555bdf9ccc47
alik <alik.ozmanov@hotmail.com>
2024-02-16 10:26:31 +0100

    java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,12 +6,16 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 public class MainFrame extends JFrame {
+    // Définition de la police principale
     final private Font mainFont = new Font("Segeo print", Font.BOLD, 18);
+    // Déclaration des champs de texte et du label
     JTextField tfFirstName, tfLastName;
     JLabel lbWelcome;
 
+    // Méthode pour initialiser la fenêtre
     public void initialize() {
         /************ Form Panel ************/
+        // Création des composants pour le formulaire
         JLabel lbFirstName = new JLabel("First Name");
         lbFirstName.setFont(mainFont);
 
@@ -24,6 +28,7 @@ public class MainFrame extends JFrame {
         tfLastName = new JTextField();
         tfLastName.setFont(mainFont);
 
+        // Création du panneau de formulaire
         JPanel formPanel = new JPanel();
         formPanel.setLayout(new GridLayout(4, 1, 5, 5));
         formPanel.setOpaque(false);
@@ -32,46 +37,45 @@ public class MainFrame extends JFrame {
         formPanel.add(lbLastName);
         formPanel.add(tfLastName);
 
-        /************ Form Panel ************/
+        /************ Welcome Label ************/
+        // Création du label de bienvenue
         lbWelcome = new JLabel();
         lbWelcome.setFont(mainFont);
 
         /************ Buttons Panel ************/
+        // Création des boutons et ajout des écouteurs d'événements
         JButton btnOK = new JButton("OK");
         btnOK.setFont(mainFont);
         btnOK.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
+                // Traitement lorsque le bouton OK est cliqué
                 String FirstName = tfFirstName.getText();
                 String LastName = tfLastName.getText();
                 lbWelcome.setText("Hello " + FirstName + " " + LastName);
             }
-            
         });
 
         JButton btnClear = new JButton("Clear");
         btnClear.setFont(mainFont);
-        btnClear.addActionListener( new ActionListener() {
-
+        btnClear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
+                // Traitement lorsque le bouton Clear est cliqué
                 tfFirstName.setText("");
                 tfLastName.setText("");
                 lbWelcome.setText("");
             }
-            
         });
 
+        // Création du panneau de boutons
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
         buttonsPanel.setOpaque(false);
         buttonsPanel.add(btnOK);
         buttonsPanel.add(btnClear);
 
-        
+        // Création du panneau principal
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.setBackground(new Color(128, 128, 255));
@@ -80,8 +84,10 @@ public class MainFrame extends JFrame {
         mainPanel.add(lbWelcome, BorderLayout.CENTER);
         mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
 
+        // Ajout du panneau principal à la fenêtre
         add(mainPanel);
 
+        // Configuration de la fenêtre
         setTitle("Welcome");
         setSize(500, 600);
         setMinimumSize(new Dimension(300, 400));
@@ -89,6 +95,7 @@ public class MainFrame extends JFrame {
         setVisible(true);
     }
 
+    // Méthode principale
     public static void main(String[] args) {
         MainFrame myFrame = new MainFrame();
         myFrame.initialize();
